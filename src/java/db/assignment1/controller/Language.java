ef6912b2eef5892e1a2145dc9a1c9a8f285d4cbd
Sudip Lama <sudiplama20@gmail.com>
2016-03-12 19:54:24 -0500

    Added A Internationalization support to Project: Adedd Language.jave class for controlling Language En_en_US.properties for english language properties file NP_en_US.properties for Neplai Language properties files

---
+++ src/java/db/assignment1/controller/Language.java
@@ -0,0 +1,68 @@
+package db.assignment1.controller;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
+
+@ManagedBean(name = "Language")
+@SessionScoped
+public class Language implements Serializable {
+
+    private static final long serialVersionUID = 20140401004L;
+    private List<SelectItem> languageList;
+    private String selectedLanguage = "EN";
+    private static ResourceBundle valueOf;
+    private final static Map languageMap = new HashMap();
+    private final Locale defaultLocale = new Locale("en", "US");
+
+    public Language() {
+        languageMap.put("English", "EN");
+        languageMap.put("नेपाली", "NP");
+    }
+
+    public void onLanguageChange() {
+        valueOf = null;
+    }
+
+    public List<SelectItem> getLanguageList() {
+        if (languageList == null) {
+            languageList = new ArrayList<>();
+            SelectItem tempSelectItem;
+            for (Object tempObject : languageMap.keySet()) {
+                tempSelectItem = new SelectItem(languageMap.get(tempObject).toString(), tempObject.toString());
+                languageList.add(tempSelectItem);
+            }
+        }
+        return languageList;
+    }
+
+    public void setLanguageList(List<SelectItem> languageList) {
+        this.languageList = languageList;
+    }
+
+    public String getSelectedLanguage() {
+        return selectedLanguage;
+    }
+
+    public void setSelectedLanguage(String selectedLanguage) {
+        this.selectedLanguage = selectedLanguage;
+    }
+
+    public ResourceBundle getValueOf() throws Exception {
+        if (valueOf == null) {
+            valueOf = ResourceBundle.getBundle(selectedLanguage, defaultLocale);
+        }
+        return valueOf;
+    }
+
+    public void setValueOf(ResourceBundle valueOf) {
+        Language.valueOf = valueOf;
+    }
+}
